c9a9c9f32f1140f577aef7048e3d4202b4fa9b89
Gracie Liu-Fang <gracie@architizer.com>
2018-11-14 17:32:05 -0500

    Add name line string concat

--- notes.js
+++ notes.js
@@ -1,13 +1,17 @@
 // ts -> date string: 
-
 const months = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
-
 const ts = 1337774582
 const date = new Date(ts)
 const year = date.getFullYear()
 const day = date.getDate()
 const month = months[date.getMonth()]
-
 const activeDate = `active since ${month} ${day}, ${year}`
+
+// user line:
+const username = 'marymeeker'
+const realname = 'Mary Meeker'
+const userLine = `${realname} - @${username}`
+
+// 
